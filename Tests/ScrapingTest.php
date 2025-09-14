3e1bfe48059456ecd71857ac8f6b0ecce09359c9
Shesh Ghimire <sheshgh@outlook.com>
2025-09-14 16:20:37 +0545

    new `PaymentCardFactory` based on scraper factory.

--- Tests/ScrapingTest.php
+++ Tests/ScrapingTest.php
@@ -5,17 +5,14 @@ namespace TheWebSolver\Codegarage\Test;
 
 use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\Attributes\Test;
-use TheWebSolver\Codegarage\Scraper\Factory;
-use TheWebSolver\Codegarage\PaymentCard\Tracer\WikiPaymentCardsTracer;
-use TheWebSolver\Codegarage\PaymentCard\Service\CardNumberScrapingService;
+use TheWebSolver\Codegarage\PaymentCard\PaymentCardFactory;
 
 class ScrapingTest extends TestCase {
 	public const WIKI_CARDS = __DIR__ . DIRECTORY_SEPARATOR . 'Resource' . DIRECTORY_SEPARATOR . 'wiki-cards.php';
 
 	#[Test]
 	public function itParsesScrapedPaymentCardDetailsFromWikiSite(): void {
-		$factory  = new Factory( new CardNumberScrapingService( new WikiPaymentCardsTracer() ) );
-		$iterator = $factory->generate();
+		$iterator = ( new PaymentCardFactory() )->generateRowIterator();
 
 		foreach ( require_once self::WIKI_CARDS as $expectedCard ) {
 			$this->assertSame( $expectedCard, $iterator->current()->getArrayCopy() );
